0ce92114621f711d63c648e305ffa8ba17fc32bf
Elvina Ganieva <elvinaganieva5411@yandex.ru>
2021-09-04 16:41:34 +0300

    add verify and @beforeEach and @afterEach

--- qa/src/test/ru/sber/qa/ScannerTest.kt
+++ qa/src/test/ru/sber/qa/ScannerTest.kt
@@ -1,20 +1,39 @@
 package ru.sber.qa
 
-import io.mockk.every
-import io.mockk.mockkObject
-import org.junit.jupiter.api.Test
+import io.mockk.*
+import org.junit.jupiter.api.*
 
 import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.assertDoesNotThrow
-import org.junit.jupiter.api.assertThrows
 import kotlin.random.Random
 
 internal class ScannerTest {
 
+    @BeforeEach
+    fun setUp() {
+        mockkObject(Random.Default)
+    }
+
+    @Test
+    fun `getScanData() should return byteArray`() {
+        //given
+        every { Random.Default.nextLong(5000L, 15000L) } returns 7000L
+
+        //when
+        assertDoesNotThrow("Should not throw an exception") {
+            Scanner.getScanData()
+        }
+
+        //then
+        verify(exactly = 1) {
+            Random.Default.nextLong(5000L, 15000L)
+            Random.Default.nextBytes(100)
+        }
+        confirmVerified(Random.Default)
+    }
+
     @Test
     fun `getScanData() should throw exception`() {
         //given
-        mockkObject(Random.Default)
         every { Random.Default.nextLong(5000L, 15000L) } returns 11000L
 
         //when
@@ -24,19 +43,17 @@ internal class ScannerTest {
 
         //then
         assertEquals("Таймаут сканирования документа", exception.message)
+        verify(exactly = 1) {
+            Random.Default.nextLong(5000L, 15000L)
+        }
+        verify(exactly = 0) {
+            Random.Default.nextBytes(100)
+        }
+        confirmVerified(Random.Default)
     }
 
-    @Test
-    fun `getScanData() should return byteArray`() {
-        //given
-        mockkObject(Random.Default)
-        every { Random.Default.nextLong(5000L, 15000L) } returns 7000L
-
-        //when
-        assertDoesNotThrow("Should not throw an exception") {
-            Scanner.getScanData()
-        }
-
-        //then
+    @AfterEach
+    fun tearDown() {
+        unmockkAll()
     }
 }
