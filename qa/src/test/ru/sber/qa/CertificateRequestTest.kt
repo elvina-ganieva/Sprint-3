67faaaad2401621d0b950078c43d1ecb09441fd6
Elvina Ganieva <elvinaganieva5411@yandex.ru>
2021-09-04 14:26:36 +0300

    add Scanner class and CertificateRequest class test

--- qa/src/test/ru/sber/qa/CertificateRequestTest.kt
+++ qa/src/test/ru/sber/qa/CertificateRequestTest.kt
@@ -1,12 +1,27 @@
 package ru.sber.qa
 
+import io.mockk.*
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
+import kotlin.random.Random
 
 internal class CertificateRequestTest {
 
+    private val certificateRequest = CertificateRequest(12345, CertificateType.NDFL)
+
     @Test
-    fun process() {
+    fun `process() should return initialized certificate`() {
+        //given
+        mockkObject(Random.Default)
+        every { Random.Default.nextLong(5000L, 15000L) } returns 7000L
+
+        //when
+        val certificate = certificateRequest.process(1)
+
+        //then
+        assertEquals(1, certificate.processedBy)
+        assertEquals(certificateRequest, certificate.certificateRequest)
     }
+
 }
--- qa/src/test/ru/sber/qa/ScannerTest.kt
+++ qa/src/test/ru/sber/qa/ScannerTest.kt
@@ -1,12 +1,42 @@
 package ru.sber.qa
 
+import io.mockk.every
+import io.mockk.mockkObject
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
+import kotlin.random.Random
 
 internal class ScannerTest {
 
     @Test
-    fun getScanData() {
+    fun `getScanData() should throw exception`() {
+        //given
+        mockkObject(Random.Default)
+        every { Random.Default.nextLong(5000L, 15000L) } returns 11000L
+
+        //when
+        val exception = assertThrows<ScanTimeoutException> {
+            Scanner.getScanData()
+        }
+
+        //then
+        assertEquals("Таймаут сканирования документа", exception.message)
+    }
+
+    @Test
+    fun `getScanData() should return byteArray`() {
+        //given
+        mockkObject(Random.Default)
+        every { Random.Default.nextLong(5000L, 15000L) } returns 7000L
+
+        //when
+        assertDoesNotThrow("Should not throw an exception") {
+            Scanner.getScanData()
+        }
+
+        //then
     }
 }
