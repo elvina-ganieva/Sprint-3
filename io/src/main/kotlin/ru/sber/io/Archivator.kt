dd347bca438f0f9b56c3c6e3ff60f5996881fdd8
Elvina Ganieva <elvinaganieva5411@yandex.ru>
2021-09-08 16:30:29 +0300

    finish Archivator

--- io/src/main/kotlin/ru/sber/io/Archivator.kt
+++ io/src/main/kotlin/ru/sber/io/Archivator.kt
@@ -1,5 +1,13 @@
 package ru.sber.io
 
+import java.io.BufferedInputStream
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.util.zip.ZipEntry
+import java.util.zip.ZipInputStream
+import java.util.zip.ZipOutputStream
+
+
 /**
  * Реализовать методы архивации и разархивации файла.
  * Для реализиации использовать ZipInputStream и ZipOutputStream.
@@ -11,7 +19,29 @@ class Archivator {
      * Архив должен располагаться в том же каталоге, что и исходной файл.
      */
     fun zipLogfile() {
+        val sourceFilename = "io/logfile.log"
+        val targetFilename = "io/unzippedLogfile.zip"
+        var entry: ZipEntry
+        var buffer: ByteArray
 
+        try {
+            FileInputStream(sourceFilename).use { fis ->
+                FileOutputStream(targetFilename).use { fos ->
+                    ZipOutputStream(fos).use { zos ->
+
+                        buffer = ByteArray(fis.available())
+                        fis.read(buffer)
+
+                        entry = ZipEntry(sourceFilename)
+                        zos.putNextEntry(entry)
+                        zos.write(buffer)
+                        zos.closeEntry()
+                    }
+                }
+            }
+        } catch (t: Throwable) {
+            throw t
+        }
     }
 
     /**
@@ -19,6 +49,31 @@ class Archivator {
      * Извлечь из архива logfile.zip файл и сохарнить его в том же каталоге с именем unzippedLogfile.log
      */
     fun unzipLogfile() {
+        val sourceFilename = "io/unzippedLogfile.zip"
+        val targetFilename = "io/unzippedLogfile.log"
+        val buffer = ByteArray(1024)
+        var count: Int
+        var zipEntry: ZipEntry?
 
+        try {
+            FileInputStream(sourceFilename).use { fis ->
+                ZipInputStream(BufferedInputStream(fis)).use { zis ->
+                    zipEntry = zis.nextEntry
+                    if (zipEntry != null) {
+                        FileOutputStream(targetFilename).use { fos ->
+
+                            count = zis.read(buffer)
+                            while (count != -1) {
+                                fos.write(buffer, 0, count)
+                                count = zis.read(buffer)
+                            }
+                            zis.closeEntry()
+                        }
+                    }
+                }
+            }
+        } catch (t: Throwable) {
+            throw t
+        }
     }
 }
